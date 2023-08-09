2d4aed2917d2c571dbdca3708243b88ff9393cc5
Георги Йорданов <gyordanov94@abv.bg>
2023-08-09 15:56:59 +0300

    add first aplications, urls, view, html and css files

--- MyPetstagramProject/urls.py
+++ MyPetstagramProject/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', include('MyPetstagramProject.common.urls')),
+    path('accounts/', include('MyPetstagramProject.accounts.urls')),
+    path('pets/', include('MyPetstagramProject.pets.urls')),
+    path('photos/', include('MyPetstagramProject.photos.urls')),
 ]
