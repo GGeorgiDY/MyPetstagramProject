06a34ee1e5cc375a88ed9433dbf7a8a2338d96e1
Георги Йорданов <gyordanov94@abv.bg>
2023-08-11 02:34:47 +0300

    create comment form, comment view and fix comment section in html

---
+++ MyPetstagramProject/common/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from MyPetstagramProject.common.models import PhotoComment
+
+
+class PhotoCommentForm(forms.ModelForm):
+    class Meta:
+        model = PhotoComment
+        fields = ('text',)
+        widgets = {
+            'text': forms.Textarea(
+                attrs={
+                    'cols': 40,
+                    'rows': 10,
+                    'placeholder': 'Add comment...',
+                }
+            ),
+        }
--- MyPetstagramProject/common/views.py
+++ MyPetstagramProject/common/views.py
@@ -1,6 +1,7 @@
 import pyperclip
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from MyPetstagramProject.common.forms import PhotoCommentForm
 from MyPetstagramProject.common.models import PhotoLike
 from MyPetstagramProject.common.utils import get_photo_url
 from MyPetstagramProject.core.photo_utils import apply_likes_count, apply_user_liked_photo
@@ -12,6 +13,7 @@ def index(request):
     photos = [apply_user_liked_photo(photo) for photo in photos]
     context = {
         'photos': photos,
+        'comment_form': PhotoCommentForm(),
     }
 
     return render(request, 'common/home-page.html', context)
@@ -34,3 +36,18 @@ def share_photo(request, photo_id):
     photo_details_url = reverse('details photo', kwargs={'pk': photo_id})
     pyperclip.copy(photo_details_url)
     return redirect(get_photo_url(request, photo_id))
+
+
+def comment_photo(request, photo_id):
+    photo = Photo.objects.filter(pk=photo_id).get()
+    # тук не е необходимо да си правим if == GET понеже тук имаме само POST метод
+    form = PhotoCommentForm(request.POST)
+
+    # ако е валидна формата, тогава запази коментара
+    if form.is_valid():
+        # правейки го по този начин
+        comment = form.save(commit=False)   # does not persist to DB
+        comment.photo = photo
+        comment.save()
+
+    return redirect('index')
