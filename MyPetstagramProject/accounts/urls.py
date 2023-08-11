cc962585b20658df6811adb3e57a7a59f9567950
Георги Йорданов <gyordanov94@abv.bg>
2023-08-11 17:21:11 +0300

    add UserDetailsView and fix profile-details-page.html

--- MyPetstagramProject/accounts/urls.py
+++ MyPetstagramProject/accounts/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path, include
-from MyPetstagramProject.accounts.views import SignInView, details_user, edit_user, delete_user, \
-    SignUpView, SignOutView
+from MyPetstagramProject.accounts.views import SignInView, edit_user, delete_user, \
+    SignUpView, SignOutView, UserDetailsView
 
 urlpatterns = (
     path('login/', SignInView.as_view(), name='login user'),
     path('register/', SignUpView.as_view(), name='register user'),
     path('logout/', SignOutView.as_view(), name='logout user'),
     path('profile/<int:pk>/', include([
-        path('', details_user, name='details user'),
+        path('', UserDetailsView.as_view(), name='details user'),
         path('edit/', edit_user, name='edit user'),
         path('delete/', delete_user, name='delete user'),
     ])),
--- MyPetstagramProject/accounts/views.py
+++ MyPetstagramProject/accounts/views.py
@@ -23,13 +23,32 @@ class SignOutView(auth_views.LogoutView):
     next_page = reverse_lazy('index')    # като се logout-нем къде искаме да отидем
 
 
-def delete_user(request, pk):
-    return render(request, 'accounts/profile-delete-page.html')
+class UserDetailsView(views.DetailView):
+    template_name = 'accounts/profile-details-page.html'
+    model = UserModel
 
+    # искаме да добавим допълниелни неща в контекста. Това го правим така:
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_owner'] = self.request.user == self.object
+        # self.request.user е логнатия юзър
+        # self.object е селектирания юзър
+        # context['pets_count'] = self.object.pet_set.count()
+        # context['photos_count'] = self.object.photo_set.count()
 
-def details_user(request, pk):
-    return render(request, 'accounts/profile-details-page.html')
+        # Photo.objects.select_related()  # взима тези обекти които имат foreign key към текущия обект. Връща QuerrySet
+        # Photo.objects.prefetch_related()    # взими свързаните по foreign key неща. Използва се за Many-to-one и many-to-many релации.
+
+        # photos = self.object.photo_set.prefetch_related('photolike_set') # искаме за всичките photos да им вземещ техните photolikes предварително
+
+        # context['likes_count'] = sum(x.photolike_set.count() for x in photos)
+
+        return context
 
 
 def edit_user(request, pk):
     return render(request, 'accounts/profile-edit-page.html')
+
+
+def delete_user(request, pk):
+    return render(request, 'accounts/profile-delete-page.html')
